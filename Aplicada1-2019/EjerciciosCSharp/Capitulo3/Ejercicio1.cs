bd1eb38140b3b1b5f83dd8d505bd1235622be6f6
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-05-08 19:30:29 -0400

    Ejercicio 1 cap 3 terminado

---
+++ Aplicada1-2019/EjerciciosCSharp/Capitulo3/Ejercicio1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicada1_2019.EjerciciosCSharp.Capitulo3
+{
+    public class Ejercicio1
+    {
+        static void Main(String[] args)
+        {
+            int valor;
+            Console.WriteLine.("Ingrese un numero para saber si es par o impar");
+            valor = Convert.ToInt16(Console.ReadLine());
+
+            if ((valor % 2) == 0)
+                Console.WriteLine("Es par");
+            else
+                Console.WriteLine("Es impar");
+            Console.Read();
+        }
+    }
+}
