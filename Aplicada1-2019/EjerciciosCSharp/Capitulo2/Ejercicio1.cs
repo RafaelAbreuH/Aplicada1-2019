9e441c0d5d42334f5556c6a78e9a47082a235ee8
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-05-08 01:45:55 -0400

    Ejercicio 1 Cap 2 terminado

---
+++ Aplicada1-2019/EjerciciosCSharp/Capitulo2/Ejercicio1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicada1_2019.EjerciciosCSharp.Capitulo2
+{
+    public class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            float Nlados;
+            float Vlados;
+            string valor;
+            float perimetro;
+
+            Console.WriteLine("Digite Los lados del angulo:");
+            valor = Console.ReadLine();
+            Nlados = Convert.ToSingle(valor);
+
+            Console.WriteLine("Digite el valor de los lados: ");
+            valor = Console.ReadLine();
+            Vlados = Convert.ToSingle(valor);
+
+            perimetro = Nlados * Vlados;
+
+            Console.WriteLine("El perimetro es: {0} ", perimetro);
+            Console.Read();
+        }
+    }
+}
