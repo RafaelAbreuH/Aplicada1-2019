d6e427db81e78a165f1d25cc6ac3014b3fbe04a8
Rafael Abreu Hernandez <rafael_abreu5@ucne.edu.do>
2019-05-08 20:01:17 -0400

    Ejercicio 2 cap 4 terminado

---
+++ Aplicada1-2019/EjerciciosCSharp/Capitulo4/Ejercicio2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicada1_2019.EjerciciosCSharp.Capitulo4
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+            double resultado, numero;
+            int value;
+
+            Console.WriteLine("Ingrese un numero: ");
+            numero = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese a que potencia lo quiere elevar: ");
+            value = Convert.ToInt16(Console.ReadLine());
+
+            resultado = Math.Pow(numero, value);
+
+            Console.WriteLine("La potencia de " + numero + " elevada a " + value + " potencia es " + resultado);
+            Console.ReadLine();
+
+
+
+        }
+    }
